372b5308a6f2fffc694ea8b6f813875cfe000147
Stephen Day <stephen.day@docker.com>
2025-05-05 16:54:16 -0700

    secrets: refactor into packages
    Signed-off-by: Stephen Day <stephen.day@docker.com>

---
+++ pkg/engine/engine.go
@@ -0,0 +1,104 @@
+package engine
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"slices"
+	"time"
+
+	"github.com/docker/secrets-engine/pkg/handlers"
+	"github.com/docker/secrets-engine/pkg/secrets"
+)
+
+type Engine struct {
+	plugins []pluginRegistration
+}
+
+func NewEngine() *Engine {
+	return &Engine{}
+}
+
+func (e *Engine) GetSecret(req secrets.Request) (secrets.Envelope, error) {
+	var errs []error
+
+	if err := req.ID.Valid(); err != nil {
+		return envelopeErr(req, err), err
+	}
+
+	for _, plugin := range e.plugins {
+		if req.Provider != "" && req.Provider != plugin.name {
+			continue
+		}
+		if !req.ID.Match(plugin.pattern) {
+			continue
+		}
+
+		envelope, err := plugin.provider.GetSecret(req)
+		if err != nil {
+			errs = append(errs, err)
+			continue
+		}
+
+		// we use the first matching, successful registration to resolve the secret.
+		envelope.Provider = plugin.name
+
+		if envelope.ResolvedAt.IsZero() {
+			envelope.ResolvedAt = time.Now().UTC()
+		}
+		return envelope, nil
+	}
+
+	var err error
+	if len(errs) == 0 {
+		err = fmt.Errorf("secret %q: %w", req.ID, secrets.ErrNotFound)
+	} else {
+		err = errors.Join(errs...)
+	}
+	return envelopeErr(req, err), err
+}
+
+func envelopeErr(req secrets.Request, err error) secrets.Envelope {
+	return secrets.Envelope{ID: req.ID, ResolvedAt: time.Now(), Error: err.Error()}
+}
+
+type pluginRegistration struct {
+	name     string
+	version  string
+	pattern  string
+	provider secrets.Resolver
+}
+
+func (e *Engine) Register(name, version, pattern string, provider secrets.Resolver) error {
+	if slices.ContainsFunc(e.plugins, func(pr pluginRegistration) bool {
+		return pr.name == name
+	}) {
+		return fmt.Errorf("provider name %q already registered", name)
+	}
+
+	e.plugins = append(e.plugins, pluginRegistration{
+		name:     name,
+		pattern:  pattern,
+		provider: provider,
+	})
+
+	return nil
+}
+
+func (e *Engine) RegisterHandler() (string, http.Handler) {
+	return "/secrets/register", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		panic("not implemented")
+	})
+}
+
+func (e *Engine) Handler() (string, http.Handler) {
+	mux := http.NewServeMux()
+
+	mux.Handle(handlers.Resolver(e))
+	mux.Handle(e.RegisterHandler())
+	mux.Handle("/secrets", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "ok")
+	}))
+
+	return "/", mux
+}
---
+++ pkg/handlers/api.go
@@ -0,0 +1,54 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/docker/secrets-engine/pkg/secrets"
+)
+
+func Resolver(provider secrets.Resolver) (string, http.Handler) {
+	return "/secrets/resolve", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if err := r.ParseForm(); err != nil {
+			http.Error(w, fmt.Sprintf("parsing request: %v", err), http.StatusBadRequest)
+			return
+		}
+
+		ids, ok := r.Form["id"]
+		if !ok {
+			http.Error(w, "at least one id parameter required", http.StatusBadRequest)
+			return
+		}
+
+		// TODO: check token to get what the client actually has access to
+
+		var candidates []secrets.Envelope
+		for _, idUnsafe := range ids {
+			id, err := secrets.ParseID(idUnsafe)
+			if err != nil {
+				candidates = append(candidates, secrets.Envelope{ID: secrets.ID(idUnsafe), Error: fmt.Sprintf("invalid identifier: %q", idUnsafe)})
+				continue
+			}
+			envelope, err := provider.GetSecret(secrets.Request{ID: id})
+			if err != nil {
+				candidates = append(candidates, secrets.Envelope{ID: secrets.ID(idUnsafe), Error: fmt.Sprintf("secret %s not available: %v", id, err)})
+				continue
+			}
+
+			candidates = append(candidates, envelope)
+		}
+
+		var secretsResponse struct {
+			Secrets []secrets.Envelope
+		}
+		secretsResponse.Secrets = candidates
+
+		enc := json.NewEncoder(w)
+		if err := enc.Encode(secretsResponse); err != nil {
+			http.Error(w, fmt.Sprintf("encoding response failed: %v", err), http.StatusInternalServerError)
+			return
+
+		}
+	})
+}
