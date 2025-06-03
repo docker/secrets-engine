941da0888994e6bf7c4f771385da46371ffbe92a
Johannes Großmann <grossmann.johannes@t-online.de>
2025-06-03 16:17:48 +0200

    feat(NRI): initial dummy NRI plugin

---
+++ cmd/nri-plugin/plugin.go
@@ -0,0 +1,198 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"log/slog"
+	"os"
+
+	"github.com/containerd/nri/pkg/api"
+	"github.com/containerd/nri/pkg/stub"
+	"sigs.k8s.io/yaml"
+)
+
+type config struct {
+	CfgParam1 string `json:"cfgParam1"`
+}
+
+type plugin struct {
+	stub stub.Stub
+	mask stub.EventMask //nolint:golint,unused
+}
+
+var (
+	cfg config
+)
+
+var (
+	_ = (stub.ConfigureInterface)((*plugin)(nil))
+	_ = (stub.SynchronizeInterface)((*plugin)(nil))
+	_ = (stub.ShutdownInterface)((*plugin)(nil))
+	_ = (stub.RunPodInterface)((*plugin)(nil))
+	_ = (stub.StopPodInterface)((*plugin)(nil))
+	_ = (stub.RemovePodInterface)((*plugin)(nil))
+	_ = (stub.CreateContainerInterface)((*plugin)(nil))
+	_ = (stub.PostCreateContainerInterface)((*plugin)(nil))
+	_ = (stub.StartContainerInterface)((*plugin)(nil))
+	_ = (stub.PostStartContainerInterface)((*plugin)(nil))
+	_ = (stub.UpdateContainerInterface)((*plugin)(nil))
+	_ = (stub.PostUpdateContainerInterface)((*plugin)(nil))
+	_ = (stub.StopContainerInterface)((*plugin)(nil))
+	_ = (stub.RemoveContainerInterface)((*plugin)(nil))
+)
+
+func (p *plugin) Configure(_ context.Context, config, runtime, version string) (stub.EventMask, error) {
+	slog.Info(fmt.Sprintf("Connected to %s/%s...", runtime, version))
+
+	if config == "" {
+		return 0, nil
+	}
+
+	err := yaml.Unmarshal([]byte(config), &cfg)
+	if err != nil {
+		return 0, fmt.Errorf("failed to parse configuration: %w", err)
+	}
+
+	slog.Info(fmt.Sprintf("Got configuration data %+v...", cfg))
+
+	return 0, nil
+}
+
+func (p *plugin) Synchronize(_ context.Context, pods []*api.PodSandbox, containers []*api.Container) ([]*api.ContainerUpdate, error) {
+	slog.Info(fmt.Sprintf("Synchronized state with the runtime (%d pods, %d containers)...",
+		len(pods), len(containers)))
+	return nil, nil
+}
+
+func (p *plugin) Shutdown(_ context.Context) {
+	slog.Info("Runtime shutting down...")
+}
+
+func (p *plugin) RunPodSandbox(_ context.Context, pod *api.PodSandbox) error {
+	slog.Info(fmt.Sprintf("Started pod %s/%s...", pod.GetNamespace(), pod.GetName()))
+	return nil
+}
+
+func (p *plugin) StopPodSandbox(_ context.Context, pod *api.PodSandbox) error {
+	slog.Info(fmt.Sprintf("Stopped pod %s/%s...", pod.GetNamespace(), pod.GetName()))
+	return nil
+}
+
+func (p *plugin) RemovePodSandbox(_ context.Context, pod *api.PodSandbox) error {
+	slog.Info(fmt.Sprintf("Removed pod %s/%s...", pod.GetNamespace(), pod.GetName()))
+	return nil
+}
+
+func (p *plugin) CreateContainer(_ context.Context, pod *api.PodSandbox, ctr *api.Container) (*api.ContainerAdjustment, []*api.ContainerUpdate, error) {
+	slog.Info(fmt.Sprintf("Creating container %s/%s/%s...", pod.GetNamespace(), pod.GetName(), ctr.GetName()))
+
+	//
+	// This is the container creation request handler. Because the container
+	// has not been created yet, this is the lifecycle event which allows you
+	// the largest set of changes to the container's configuration, including
+	// some of the later immutable parameters. Take a look at the adjustment
+	// functions in pkg/api/adjustment.go to see the available controls.
+	//
+	// In addition to reconfiguring the container being created, you are also
+	// allowed to update other existing containers. Take a look at the update
+	// functions in pkg/api/update.go to see the available controls.
+	//
+
+	adjustment := &api.ContainerAdjustment{}
+	updates := []*api.ContainerUpdate{}
+
+	return adjustment, updates, nil
+}
+
+func (p *plugin) PostCreateContainer(_ context.Context, pod *api.PodSandbox, ctr *api.Container) error {
+	slog.Info(fmt.Sprintf("Created container %s/%s/%s...", pod.GetNamespace(), pod.GetName(), ctr.GetName()))
+	return nil
+}
+
+func (p *plugin) StartContainer(_ context.Context, pod *api.PodSandbox, ctr *api.Container) error {
+	slog.Info(fmt.Sprintf("Starting container %s/%s/%s...", pod.GetNamespace(), pod.GetName(), ctr.GetName()))
+	return nil
+}
+
+func (p *plugin) PostStartContainer(_ context.Context, pod *api.PodSandbox, ctr *api.Container) error {
+	slog.Info(fmt.Sprintf("Started container %s/%s/%s...", pod.GetNamespace(), pod.GetName(), ctr.GetName()))
+	return nil
+}
+
+func (p *plugin) UpdateContainer(_ context.Context, pod *api.PodSandbox, ctr *api.Container, _ *api.LinuxResources) ([]*api.ContainerUpdate, error) {
+	slog.Info(fmt.Sprintf("Updating container %s/%s/%s...", pod.GetNamespace(), pod.GetName(), ctr.GetName()))
+
+	//
+	// This is the container update request handler. You can make changes to
+	// the container update before it is applied. Take a look at the functions
+	// in pkg/api/update.go to see the available controls.
+	//
+	// In addition to altering the pending update itself, you are also allowed
+	// to update other existing containers.
+	//
+
+	updates := []*api.ContainerUpdate{}
+
+	return updates, nil
+}
+
+func (p *plugin) PostUpdateContainer(_ context.Context, pod *api.PodSandbox, ctr *api.Container) error {
+	slog.Info(fmt.Sprintf("Updated container %s/%s/%s...", pod.GetNamespace(), pod.GetName(), ctr.GetName()))
+	return nil
+}
+
+func (p *plugin) StopContainer(_ context.Context, pod *api.PodSandbox, ctr *api.Container) ([]*api.ContainerUpdate, error) {
+	slog.Info(fmt.Sprintf("Stopped container %s/%s/%s...", pod.GetNamespace(), pod.GetName(), ctr.GetName()))
+
+	//
+	// This is the container (post-)stop request handler. You can update any
+	// of the remaining running containers. Take a look at the functions in
+	// pkg/api/update.go to see the available controls.
+	//
+
+	return []*api.ContainerUpdate{}, nil
+}
+
+func (p *plugin) RemoveContainer(_ context.Context, pod *api.PodSandbox, ctr *api.Container) error {
+	slog.Info(fmt.Sprintf("Removed container %s/%s/%s...", pod.GetNamespace(), pod.GetName(), ctr.GetName()))
+	return nil
+}
+
+func (p *plugin) onClose() {
+	slog.Info("Connection to the runtime lost, exiting...")
+	os.Exit(0)
+}
+
+func main() {
+	var (
+		pluginName string
+		pluginIdx  string
+		err        error
+	)
+
+	flag.StringVar(&pluginName, "name", "", "plugin name to register to NRI")
+	flag.StringVar(&pluginIdx, "idx", "", "plugin index to register to NRI")
+	flag.Parse()
+
+	p := &plugin{}
+	opts := []stub.Option{
+		stub.WithOnClose(p.onClose),
+	}
+	if pluginName != "" {
+		opts = append(opts, stub.WithPluginName(pluginName))
+	}
+	if pluginIdx != "" {
+		opts = append(opts, stub.WithPluginIdx(pluginIdx))
+	}
+
+	if p.stub, err = stub.New(p, opts...); err != nil {
+		log.Fatalf("failed to create plugin stub: %v", err)
+	}
+
+	if err = p.stub.Run(context.Background()); err != nil {
+		slog.Error(fmt.Sprintf("plugin exited (%v)", err))
+		os.Exit(1)
+	}
+}
--- go.mod
+++ go.mod
@@ -4,12 +4,30 @@ go 1.24.0
 
 require (
 	github.com/alecthomas/kong v1.10.0
+	github.com/containerd/nri v0.9.0
 	github.com/keybase/go-keychain v0.0.1
 	github.com/stretchr/testify v1.10.0
+	sigs.k8s.io/yaml v1.3.0
 )
 
 require (
+	github.com/containerd/log v0.1.0 // indirect
+	github.com/containerd/ttrpc v1.2.6-0.20240827082320-b5cd6e4b3287 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/knqyf263/go-plugin v0.8.1-0.20240827022226-114c6257e441 // indirect
+	github.com/opencontainers/runtime-spec v1.0.3-0.20220825212826-86290f6a00fb // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/tetratelabs/wazero v1.8.2-0.20241030035603-dc08732e57d5 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230731190214-cbb8c96f2d6d // indirect
+	google.golang.org/grpc v1.57.1 // indirect
+	google.golang.org/protobuf v1.34.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/cri-api v0.25.3 // indirect
 )
