16ed9fa48866022ff3ab0d768da9ad646e925288
Johannes Großmann <grossmann.johannes@t-online.de>
2025-06-20 16:00:15 +0200

    feat(sdk): stub plugin service

---
+++ pkg/stub/lifecycle.go
@@ -0,0 +1,21 @@
+package stub
+
+import (
+	"context"
+
+	"connectrpc.com/connect"
+
+	resolverv1 "github.com/docker/secrets-engine/pkg/api/resolver/v1"
+	"github.com/docker/secrets-engine/pkg/api/resolver/v1/resolverv1connect"
+)
+
+var _ resolverv1connect.PluginServiceHandler = &pluginService{}
+
+type pluginService struct {
+	shutdown func(context.Context)
+}
+
+func (s *pluginService) Shutdown(ctx context.Context, _ *connect.Request[resolverv1.ShutdownRequest]) (*connect.Response[resolverv1.ShutdownResponse], error) {
+	s.shutdown(ctx)
+	return connect.NewResponse(&resolverv1.ShutdownResponse{}), nil
+}
