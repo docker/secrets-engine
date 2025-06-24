cc23843d14a315f3c96ee06c6375d703a78a3a6f
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2025-06-24 12:14:34 +0200

    store/keychain: keep dummy store for linux
    Signed-off-by: Alano Terblanche <18033717+Benehiko@users.noreply.github.com>

--- store/keychain/keychain_darwin.go
+++ store/keychain/keychain_darwin.go
@@ -13,33 +13,40 @@ var (
 	ErrAuthFailed            = errors.New("user incorrectly enterered their credentials")
 )
 
-func (k *keychainStore[T]) getItem(id store.ID) kc.Item {
+func newKeychainItem[T store.Secret](id store.ID, k *keychainStore[T]) kc.Item {
 	item := kc.NewItem()
 	// generic password is used here as we don't know what we are storing
 	// the main difference between a generic and internet password is the
 	// addition of a server URL
 	item.SetSecClass(kc.SecClassGenericPassword)
-	item.SetService(k.serviceName)
-	item.SetAccessGroup(k.serviceGroup)
+	// MatchLimitOne is used to ensure we only get one item back when querying
+	// set this to MatchLimitAll if you want to retrieve all items
 	item.SetMatchLimit(kc.MatchLimitOne)
 	item.SetAccessible(kc.AccessibleAfterFirstUnlock)
 	item.SetReturnData(true)
 	item.SetReturnAttributes(true)
+
+	item.SetService(k.serviceName)
+	item.SetAccessGroup(k.serviceGroup)
+
 	if id.String() != "" {
 		item.SetLabel(k.itemLabel(id))
 		item.SetAccount(id.String())
 	}
+
 	return item
 }
 
 func (k *keychainStore[T]) Delete(ctx context.Context, id store.ID) error {
-	return mapError(kc.DeleteItem(k.getItem(id)))
+	item := newKeychainItem(id, k)
+	return mapKeychainError(kc.DeleteItem(item))
 }
 
 func (k *keychainStore[T]) Get(ctx context.Context, id store.ID) (store.Secret, error) {
-	results, err := kc.QueryItem(k.getItem(id))
+	item := newKeychainItem(id, k)
+	results, err := kc.QueryItem(item)
 	if err != nil {
-		return nil, mapError(err)
+		return nil, mapKeychainError(err)
 	}
 	if len(results) == 0 {
 		return nil, store.ErrCredentialNotFound
@@ -53,11 +60,12 @@ func (k *keychainStore[T]) Get(ctx context.Context, id store.ID) (store.Secret, 
 }
 
 func (k *keychainStore[T]) GetAll(ctx context.Context) (map[store.ID]store.Secret, error) {
-	item := k.getItem(store.ID(""))
+	item := newKeychainItem(store.ID(""), k)
 	item.SetMatchLimit(kc.MatchLimitAll)
-	results, err := kc.QueryItem(k.getItem(store.ID("")))
+
+	results, err := kc.QueryItem(item)
 	if err != nil {
-		return nil, mapError(err)
+		return nil, mapKeychainError(err)
 	}
 	creds := make(map[store.ID]store.Secret, len(results))
 	for _, result := range results {
@@ -76,12 +84,12 @@ func (k *keychainStore[T]) Save(ctx context.Context, id store.ID, secret store.S
 	if err != nil {
 		return err
 	}
-	item := k.getItem(id)
+	item := newKeychainItem(id, k)
 	item.SetData(data)
-	return kc.AddItem(item)
+	return mapKeychainError(kc.AddItem(item))
 }
 
-func mapError(err error) error {
+func mapKeychainError(err error) error {
 	if err == nil {
 		return nil
 	}
