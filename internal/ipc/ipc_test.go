1d9d9afb81cd0b37bb18cf06f997d977df6dc30d
Johannes Großmann <grossmann.johannes@t-online.de>
2025-06-27 16:06:59 +0200

    fix(sdk): simplify interface and correctly handle shutdown scenarios

--- internal/ipc/ipc_test.go
+++ internal/ipc/ipc_test.go
@@ -3,7 +3,6 @@ package ipc
 import (
 	"fmt"
 	"io"
-	"log"
 	"net"
 	"net/http"
 	"os"
@@ -19,6 +18,7 @@ const (
 )
 
 func Test_newExternalPlugin(t *testing.T) {
+	t.Parallel()
 	tests := []struct {
 		name string
 		test func(t *testing.T)
@@ -27,27 +27,15 @@ func Test_newExternalPlugin(t *testing.T) {
 			name: "ping pong on both sides",
 			test: func(t *testing.T) {
 				socketPath := filepath.Join(os.TempDir(), "secrets-engine-plugin.sock")
-				os.Remove(socketPath)
-				require.NoError(t, os.MkdirAll(filepath.Dir(socketPath), 0755))
-				l, err := net.ListenUnix("unix", &net.UnixAddr{
-					Name: socketPath,
-					Net:  "unix",
-				})
-				require.NoError(t, err)
+				l := newListener(t, socketPath)
 				doneRuntime := make(chan struct{})
 				donePlugin := make(chan struct{})
 				go func() {
 					sock, err := l.Accept()
-					if err != nil {
-						log.Fatalf("plugin accept: %v", err)
-					}
+					require.NoError(t, err)
 					defer sock.Close()
 
-					pluginHandler := http.NewServeMux()
-					pluginHandler.HandleFunc(mockPingPath, func(w http.ResponseWriter, _ *http.Request) {
-						fmt.Fprint(w, "pong-runtime")
-					})
-					i, c, err := NewRuntimeIPC(sock, pluginHandler)
+					i, c, err := NewRuntimeIPC(sock, newPingPongHandler("runtime"), nil)
 					require.NoError(t, err)
 					defer i.Close()
 					assertCommunicationToServer(t, c, "pong-plugin")
@@ -58,16 +46,10 @@ func Test_newExternalPlugin(t *testing.T) {
 				}()
 
 				sock, err := net.DialUnix("unix", nil, &net.UnixAddr{Name: socketPath, Net: "unix"})
-				if err != nil {
-					log.Fatalf("dial error: %v", err)
-				}
+				require.NoError(t, err)
 				t.Cleanup(func() { sock.Close() })
 
-				runtimeHandler := http.NewServeMux()
-				runtimeHandler.HandleFunc(mockPingPath, func(w http.ResponseWriter, _ *http.Request) {
-					fmt.Fprint(w, "pong-plugin")
-				})
-				i, c, err := NewPluginIPC(sock, runtimeHandler)
+				i, c, err := NewPluginIPC(sock, newPingPongHandler("plugin"), nil)
 				require.NoError(t, err)
 				t.Cleanup(func() { i.Close() })
 
@@ -77,12 +59,113 @@ func Test_newExternalPlugin(t *testing.T) {
 				<-doneRuntime
 			},
 		},
+		{
+			name: "stopping the plugin results in EOF on runtime side",
+			test: func(t *testing.T) {
+				socketPath := filepath.Join(os.TempDir(), "secrets-engine-plugin.sock")
+				listener := newListener(t, socketPath)
+
+				doneRuntime := make(chan struct{})
+				donePlugin := make(chan struct{})
+				go func() {
+					conn, err := listener.Accept()
+					require.NoError(t, err)
+
+					i, _, err := NewRuntimeIPC(conn, newPingPongHandler("runtime"), func(err error) {
+						assert.ErrorIs(t, err, io.EOF)
+						close(donePlugin)
+					})
+					require.NoError(t, err)
+					<-donePlugin
+					assert.NoError(t, i.Close())
+					assert.ErrorContains(t, conn.Close(), "use of closed network connection")
+					close(doneRuntime)
+				}()
+
+				sock, err := net.DialUnix("unix", nil, &net.UnixAddr{Name: socketPath, Net: "unix"})
+				require.NoError(t, err)
+
+				i, c, err := NewPluginIPC(sock, newPingPongHandler("plugin"), func(err error) {
+					assert.NoError(t, err)
+					assert.ErrorContains(t, sock.Close(), "use of closed network connection")
+				})
+				require.NoError(t, err)
+
+				assertCommunicationToServer(t, c, "pong-runtime")
+				assert.NoError(t, i.Close())
+
+				<-doneRuntime
+				assert.NoError(t, listener.Close())
+			},
+		},
+		{
+			name: "stopping the runtime results in EOF on plugin side",
+			test: func(t *testing.T) {
+				socketPath := filepath.Join(os.TempDir(), "secrets-engine-plugin.sock")
+				listener := newListener(t, socketPath)
+
+				runtimeDown := make(chan struct{})
+				pluginReady := make(chan struct{})
+				pluginDown := make(chan struct{})
+				go func() {
+					sock, err := net.DialUnix("unix", nil, &net.UnixAddr{Name: socketPath, Net: "unix"})
+					require.NoError(t, err)
+
+					done := make(chan struct{})
+					i, c, err := NewPluginIPC(sock, newPingPongHandler("plugin"), func(err error) {
+						assert.ErrorIs(t, err, io.EOF)
+						assert.ErrorContains(t, sock.Close(), "use of closed network connection")
+						close(done)
+					})
+					require.NoError(t, err)
+					assertCommunicationToServer(t, c, "pong-runtime")
+					close(pluginReady)
+					<-done
+					assert.NoError(t, i.Close())
+					close(pluginDown)
+				}()
+
+				conn, err := listener.Accept()
+				require.NoError(t, err)
+
+				i, _, err := NewRuntimeIPC(conn, newPingPongHandler("runtime"), func(err error) {
+					assert.NoError(t, err)
+					assert.ErrorContains(t, conn.Close(), "use of closed network connection")
+					close(runtimeDown)
+				})
+				require.NoError(t, err)
+				<-pluginReady
+				assert.NoError(t, i.Close())
+				<-pluginDown
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			tt.test(t)
 		})
 	}
+}
+
+func newListener(t *testing.T, socketPath string) net.Listener {
+
+	os.Remove(socketPath)
+	require.NoError(t, os.MkdirAll(filepath.Dir(socketPath), 0755))
+	l, err := net.ListenUnix("unix", &net.UnixAddr{
+		Name: socketPath,
+		Net:  "unix",
+	})
+	require.NoError(t, err)
+	t.Cleanup(func() { l.Close() })
+	return l
+}
+
+func newPingPongHandler(suffix string) http.Handler {
+	runtimeHandler := http.NewServeMux()
+	runtimeHandler.HandleFunc(mockPingPath, func(w http.ResponseWriter, _ *http.Request) {
+		fmt.Fprint(w, "pong-"+suffix)
+	})
+	return runtimeHandler
 }
 
 func assertCommunicationToServer(t *testing.T, c *http.Client, response string) {
