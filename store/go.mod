d2a1ffa17166ec272f7f24c84fb12d04c86bbb6d
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2025-06-30 11:59:24 +0200

    store/keychain: windows support
    Signed-off-by: Alano Terblanche <18033717+Benehiko@users.noreply.github.com>

--- store/go.mod
+++ store/go.mod
@@ -5,6 +5,7 @@ go 1.24.3
 replace github.com/docker/secrets-engine => ../
 
 require (
+	github.com/danieljoos/wincred v1.2.2
 	github.com/docker/secrets-engine v0.0.0-00010101000000-000000000000
 	github.com/keybase/dbus v0.0.0-20220506165403-5aa21ea2c23a
 	github.com/keybase/go-keychain v0.0.1
@@ -20,6 +21,7 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
 	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ store/keychain/keychain_windows.go
@@ -0,0 +1,160 @@
+package keychain
+
+import (
+	"context"
+	"errors"
+	"iter"
+	"strings"
+
+	"github.com/danieljoos/wincred"
+	"github.com/docker/secrets-engine/store"
+)
+
+var (
+	ErrCredentialBadUsername      = errors.New("credential username is invalid")
+	ErrInvalidCredentialFlags     = errors.New("an invalid flag was specified for the flags parameter")
+	ErrInvalidCredentialParameter = errors.New("protected field does not match provided value for an existing credential")
+	ErrNoLogonSession             = errors.New("logon session does not exist or there is no credential set associated with this logon session")
+)
+
+func (k *keychainStore[T]) Delete(ctx context.Context, id store.ID) error {
+	if err := id.Valid(); err != nil {
+		return err
+	}
+
+	g, err := wincred.GetGenericCredential(k.itemLabel(id))
+	if err != nil && !errors.Is(err, wincred.ErrElementNotFound) {
+		return mapWindowsCredentialError(err)
+	}
+	if g == nil {
+		return nil
+	}
+
+	err = g.Delete()
+	if err != nil && !errors.Is(err, wincred.ErrElementNotFound) {
+		return err
+	}
+	return nil
+}
+
+func (k *keychainStore[T]) Get(ctx context.Context, id store.ID) (store.Secret, error) {
+	if err := id.Valid(); err != nil {
+		return nil, err
+	}
+
+	g, err := wincred.GetGenericCredential(k.itemLabel(id))
+	if err != nil {
+		return nil, mapWindowsCredentialError(err)
+	}
+
+	secret := k.factory()
+	if err := secret.Unmarshal(g.CredentialBlob); err != nil {
+		return nil, err
+	}
+
+	return secret, nil
+}
+
+// isServiceCredential checks if a credential attribute contains the
+// `service:group` and `service:name` attribute.
+//
+// The keychainStore serviceGroup and serviceName should match what is stored
+// in the attributes.
+func isServiceCredential[T store.Secret](k *keychainStore[T], attrs []wincred.CredentialAttribute) bool {
+	// must have both serviceGroup and serviceName
+	var (
+		serviceName  string
+		serviceGroup string
+	)
+	for _, attr := range attrs {
+		switch attr.Keyword {
+		case "service:group":
+			serviceGroup = string(attr.Value)
+		case "service:name":
+			serviceName = string(attr.Value)
+		}
+	}
+	return strings.EqualFold(serviceGroup, k.serviceGroup) && strings.EqualFold(serviceName, k.serviceName)
+}
+
+// findServiceCredentials is an iterator that yields credentials that match the
+// service group and service name.
+func findServiceCredentials[T store.Secret](k *keychainStore[T], credentials []*wincred.Credential) iter.Seq[*wincred.Credential] {
+	return func(yield func(cred *wincred.Credential) bool) {
+		for _, c := range credentials {
+			if isServiceCredential(k, c.Attributes) {
+				if !yield(c) {
+					return
+				}
+			}
+		}
+	}
+}
+
+func (k *keychainStore[T]) GetAll(ctx context.Context) (map[store.ID]store.Secret, error) {
+	credentials, err := wincred.List()
+	if err != nil {
+		return nil, mapWindowsCredentialError(err)
+	}
+
+	onlyLabelPrefix := k.itemLabel(store.ID(""))
+
+	secrets := make(map[store.ID]store.Secret, len(credentials))
+	for cred := range findServiceCredentials(k, credentials) {
+		secret := k.factory()
+		id, err := store.ParseID(strings.ReplaceAll(cred.TargetName, onlyLabelPrefix, ""))
+		if err != nil {
+			return nil, err
+		}
+
+		gc, err := wincred.GetGenericCredential(cred.TargetName)
+		if err != nil {
+			return nil, mapWindowsCredentialError(err)
+		}
+		if err := secret.Unmarshal(gc.CredentialBlob); err != nil {
+			return nil, err
+		}
+		secrets[id] = secret
+	}
+
+	return secrets, nil
+}
+
+func (k *keychainStore[T]) Save(ctx context.Context, id store.ID, secret store.Secret) error {
+	if err := id.Valid(); err != nil {
+		return err
+	}
+
+	data, err := secret.Marshal()
+	if err != nil {
+		return err
+	}
+
+	g := wincred.NewGenericCredential(k.itemLabel(id))
+	g.UserName = id.String()
+	g.CredentialBlob = data
+	g.Persist = wincred.PersistLocalMachine
+	g.Attributes = []wincred.CredentialAttribute{
+		{
+			Keyword: "service:group",
+			Value:   []byte(k.serviceGroup),
+		},
+		{
+			Keyword: "service:name",
+			Value:   []byte(k.serviceName),
+		},
+	}
+	return mapWindowsCredentialError(g.Write())
+}
+
+func mapWindowsCredentialError(err error) error {
+	switch err {
+	case wincred.ErrElementNotFound:
+		return store.ErrCredentialNotFound
+	case wincred.ErrBadUsername:
+		return ErrCredentialBadUsername
+	case wincred.ErrInvalidParameter:
+		return ErrInvalidCredentialParameter
+	}
+	return nil
+}
