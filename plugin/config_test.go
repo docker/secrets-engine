0cb69deb2bf583104d79daa3cfd81e51ecc9b2b9
Johannes Großmann <grossmann.johannes@t-online.de>
2025-07-10 10:56:57 +0200

    refactor(sdk): move pkg/api to internal/api

--- plugin/config_test.go
+++ plugin/config_test.go
@@ -11,8 +11,8 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"github.com/docker/secrets-engine/internal/api"
 	"github.com/docker/secrets-engine/internal/ipc"
-	"github.com/docker/secrets-engine/pkg/api"
 	"github.com/docker/secrets-engine/pkg/secrets"
 )
 
