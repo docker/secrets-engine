059e6d8737ac61daccd21b65067eaffcc0f74712
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2025-07-23 14:40:46 +0200

    store/keychain(test): secret metadata on macOS
    Signed-off-by: Alano Terblanche <18033717+Benehiko@users.noreply.github.com>

--- store/keychain/keychain_darwin_test.go
+++ store/keychain/keychain_darwin_test.go
@@ -3,10 +3,63 @@
 package keychain
 
 import (
+	"bytes"
+	"context"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/docker/secrets-engine/store"
+	"github.com/docker/secrets-engine/store/mocks"
 )
+
+func TestMacosKeychain(t *testing.T) {
+	secret := &mocks.MockCredential{
+		Username: uuid.NewString(),
+		Password: "bob2",
+	}
+	var (
+		serviceName  = uuid.NewString()
+		serviceGroup = "test.testing." + uuid.NewString()
+		id           = store.ID(serviceGroup + "/" + serviceName + "/" + uuid.NewString())
+	)
+	store := keychainStore[*mocks.MockCredential]{
+		serviceGroup: "test.testing." + uuid.NewString(),
+		serviceName:  uuid.NewString(),
+		factory: func() *mocks.MockCredential {
+			return &mocks.MockCredential{}
+		},
+	}
+
+	t.Run("secret can have no attributes", func(t *testing.T) {
+		t.Cleanup(func() {
+			assert.NoError(t, store.Delete(context.Background(), id))
+		})
+		require.NoError(t, store.Save(t.Context(), id, secret))
+
+		storeSecret, err := store.Get(t.Context(), id)
+		require.NoError(t, err)
+		assert.Empty(t, storeSecret.Metadata())
+	})
+
+	t.Run("secret can store large attributes", func(t *testing.T) {
+		t.Cleanup(func() {
+			assert.NoError(t, store.Delete(context.Background(), id))
+		})
+		large := bytes.Repeat([]byte{'a'}, 1024*1024)
+		secret.Attributes = map[string]string{
+			"large": string(large),
+			"small": "eyy",
+		}
+		require.NoError(t, store.Save(t.Context(), id, secret))
+
+		storeSecret, err := store.Get(t.Context(), id)
+		require.NoError(t, err)
+		assert.EqualValues(t, secret.Attributes, storeSecret.Metadata())
+	})
+}
 
 func TestConvertAttributes(t *testing.T) {
 	t.Run("can convert attributes map into map of any", func(t *testing.T) {
