e4ff8490d208931dbe4e24cd01618d0aa1290585
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2025-08-04 07:55:43 +0200

    secrets: identifier must always be valid
    This patch enforces validation when creating a secret identifier.
    Previously it was possible to cast a string as the identifier which
    would mean any string could be an ID. This then made it so the caller
    would be responsible to check the validity of the ID.
    
    Usage of the ID inside functions such as `Match` would become ambigious
    and we would either always need to check if the ID is valid or ignore errors
    resulting from an invalid ID.
    
    It would still be possible to pass in an invalid ID by implementing the
    ID interface, however, the risk is low since we check the validity of
    IDs when parsing them from input.
    
    Signed-off-by: Alano Terblanche <18033717+Benehiko@users.noreply.github.com>

--- internal/secrets/identifiers_test.go
+++ internal/secrets/identifiers_test.go
@@ -81,3 +81,93 @@ func TestMatch(t *testing.T) {
 		})
 	}
 }
+
+func TestParseIDNew(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected error
+	}{
+		{"valid name with dash", "my-secretA9", nil},
+		{"valid name with dot", "my.secretA9", nil},
+		{"valid name with slash", "my/secret", nil},
+		{"valid name with underscore", "my_secretA9", nil},
+		{"invalid name with trailing slash", "my/secret/", ErrInvalidID{"my/secret/"}},
+		{"invalid name with leading slash", "/my/secret", ErrInvalidID{"/my/secret"}},
+		{"invalid name with empty component", "my//secret", ErrInvalidID{"my//secret"}},
+		{"invalid name with colon", "my:secret", ErrInvalidID{"my:secret"}},
+		{"invalid name with space", "my secret", ErrInvalidID{"my secret"}},
+		{"invalid name with hashtag", "my#secret", ErrInvalidID{"my#secret"}},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := ParseIDNew(tc.input)
+			assert.ErrorIs(t, err, tc.expected)
+		})
+	}
+}
+
+func TestMatchNew(t *testing.T) {
+	tests := []struct {
+		pattern  string
+		ids      []string
+		expected bool
+	}{
+		{
+			pattern:  "**",
+			ids:      []string{"foo", "foo/bar", "foo/bar/baz"},
+			expected: true,
+		},
+		{
+			pattern:  "foo/bar",
+			ids:      []string{"foo/bar/baz", "foo"},
+			expected: false,
+		},
+		{
+			pattern:  "foo/*",
+			ids:      []string{"foo/bar"},
+			expected: true,
+		},
+		{
+			pattern:  "foo/*",
+			ids:      []string{"foo/bar/baz", "foo"},
+			expected: false,
+		},
+		{
+			pattern:  "*/bar",
+			ids:      []string{"foo/bar"},
+			expected: true,
+		},
+		{
+			pattern:  "*/bar",
+			ids:      []string{"foo/bar/baz", "foo"},
+			expected: false,
+		},
+		{
+			pattern:  "foo/**/baz",
+			ids:      []string{"foo/bar/baz", "foo/baz", "foo/bar/something/baz"},
+			expected: true,
+		},
+		{
+			pattern:  "foo/**/baz",
+			ids:      []string{"foo/bar", "foo/bar/baz/qux"},
+			expected: false,
+		},
+		{
+			pattern:  "com.test.test/**",
+			ids:      []string{"com.test.test/test/bob", "com.test.test/test/alice"},
+			expected: true,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.pattern, func(t *testing.T) {
+			for _, m := range tc.ids {
+				id, err := ParseIDNew(m)
+				assert.NoError(t, err)
+				pattern, err := ParsePatternNew(tc.pattern)
+				assert.NoError(t, err)
+				assert.Equalf(t, tc.expected, id.Match(pattern), "unexpected match for id `%q` to and pattern `%q`", m, tc.pattern)
+			}
+		})
+	}
+}
