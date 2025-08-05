d422352f48b76bba4086e8490155973e41e1b814
Johannes Großmann <grossmann.johannes@t-online.de>
2025-08-05 12:03:58 +0200

    fix: version strings must be prefixed with 'v'

--- internal/api/version_test.go
+++ internal/api/version_test.go
@@ -9,12 +9,12 @@ import (
 func Test_Version(t *testing.T) {
 	t.Parallel()
 	t.Run("any semver string is a valid version", func(t *testing.T) {
-		v, err := NewVersion("1.0")
+		v, err := NewVersion("v1.0")
 		assert.NoError(t, err)
-		assert.Equal(t, "1.0", v.String())
+		assert.Equal(t, "v1.0", v.String())
 	})
 	t.Run("no v prefix", func(t *testing.T) {
-		_, err := NewVersion("v1.0.0")
+		_, err := NewVersion("1.0.0")
 		assert.ErrorIs(t, err, ErrVPrefix)
 	})
 	t.Run("no empty string", func(t *testing.T) {
