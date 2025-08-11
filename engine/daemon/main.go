b59ab4f2564352763ef97b4e44ac11be09a5f322
Johannes Großmann <grossmann.johannes@t-online.de>
2025-08-11 17:00:05 +0200

    feat(daemon): run a secrets engine

--- engine/daemon/main.go
+++ engine/daemon/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"runtime/debug"
+
+	"github.com/docker/secrets-engine/engine"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		panic("could not read build info")
+	}
+	// TODO: double check if the version actually points to the engine sub-module or the main module
+	e, err := engine.New("secrets-engine", bi.Main.Version,
+		engine.WithEngineLaunchedPluginsDisabled(),
+		engine.WithExternallyLaunchedPluginsDisabled(),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := e.Run(context.Background()); err != nil {
+		panic(err)
+	}
 }
