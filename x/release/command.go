2d725f3a4e36c93e81b1a89e9c41aebab3e93da0
Johannes Großmann <grossmann.johannes@t-online.de>
2025-09-11 10:48:20 +0200

    fix(releaser): pass "before commit hook" through

--- x/release/command.go
+++ x/release/command.go
@@ -54,9 +54,9 @@ func ReleaseCommand(cfg Config) (*cobra.Command, error) {
 				if !ok {
 					return fmt.Errorf("module %s not found", mod)
 				}
-				return helper.BumpModule(mod, opts.level, modData, &projectFS{opts: opts, logger: logging.NewDefaultLogger("")})
+				return helper.BumpModule(mod, opts.level, modData, &projectFS{opts: opts, beforeCommitHook: cfg.BeforeCommitHook, logger: logging.NewDefaultLogger("")})
 			}
-			return helper.BumpIterative(mod, opts.level, data, &projectFS{opts: opts, logger: logging.NewDefaultLogger("")})
+			return helper.BumpIterative(mod, opts.level, data, &projectFS{opts: opts, beforeCommitHook: cfg.BeforeCommitHook, logger: logging.NewDefaultLogger("")})
 		},
 	}
 
