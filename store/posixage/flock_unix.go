5d89547e163d4a282704cf5c4004d738da971cd0
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2025-09-15 11:34:13 +0200

    posixage: lock recovery pattern
    Signed-off-by: Alano Terblanche <18033717+Benehiko@users.noreply.github.com>

--- store/posixage/flock_unix.go
+++ store/posixage/flock_unix.go
@@ -9,31 +9,33 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-func lockFile(root *os.Root, exclusive bool) (unlockFunc, error) {
-	fl, err := openFile(root)
-	if err != nil {
-		return nil, err
-	}
-
+// lockFile acquires a file-based lock using a temporary file.
+//
+// The exclusive flag should be set for write or delete operations to prevent
+// concurrent reads. The timeout parameter defines how long to keep retrying
+// before giving up.
+//
+// It returns an [unlockFunc] on success which should always be called inside
+// a defer.
+func lockFile(f *os.File, exclusive bool) (unlockFunc, error) {
 	flag := unix.LOCK_NB
 	if exclusive {
 		flag |= unix.LOCK_EX
 	} else {
 		flag |= unix.LOCK_SH
 	}
-	err = unix.Flock(int(fl.Fd()), flag)
+
+	err := unix.Flock(int(f.Fd()), flag)
 	if err != nil {
-		_ = fl.Close()
 		return nil, errors.Join(ErrLockUnsuccessful, err)
 	}
 
 	return func() error {
-		if err := fl.Close(); err != nil {
-			return err
-		}
-		if err := unix.Flock(int(fl.Fd()), unix.LOCK_UN); err != nil {
+		defer func() { _ = f.Close() }()
+		if err := unix.Flock(int(f.Fd()), unix.LOCK_UN); err != nil {
 			return errors.Join(ErrUnlockUnsuccessful, err)
 		}
+
 		return nil
 	}, nil
 }
--- store/posixage/flock_windows.go
+++ store/posixage/flock_windows.go
@@ -9,27 +9,30 @@ import (
 	"golang.org/x/sys/windows"
 )
 
-func lockFile(root *os.Root, exclusive bool) (unlockFunc, error) {
-	fl, err := openFile(root)
-	if err != nil {
-		return nil, err
-	}
-
+// lockFile acquires a file-based lock using a temporary file.
+//
+// The exclusive flag should be set for write or delete operations to prevent
+// concurrent reads. The timeout parameter defines how long to keep retrying
+// before giving up.
+//
+// It returns an [unlockFunc] on success which should always be called inside
+// a defer.
+func lockFile(f *os.File, exclusive bool) (unlockFunc, error) {
 	flag := windows.LOCKFILE_FAIL_IMMEDIATELY
 	if exclusive {
 		flag |= windows.LOCKFILE_EXCLUSIVE_LOCK
 	}
+
 	var overlapped windows.Overlapped
-	err = windows.LockFileEx(windows.Handle(fl.Fd()), uint32(flag), 0, 1, 0, &overlapped)
+	err := windows.LockFileEx(windows.Handle(f.Fd()), uint32(flag), 0, 1, 0, &overlapped)
 	if err != nil {
 		return nil, errors.Join(ErrLockUnsuccessful, err)
 	}
+
 	return func() error {
-		if err := fl.Close(); err != nil {
-			return err
-		}
+		defer func() { _ = f.Close() }()
 		var overlapped windows.Overlapped
-		err := windows.UnlockFileEx(windows.Handle(fl.Fd()), 0, 1, 0, &overlapped)
+		err := windows.UnlockFileEx(windows.Handle(f.Fd()), 0, 1, 0, &overlapped)
 		if err != nil {
 			return errors.Join(ErrUnlockUnsuccessful, err)
 		}
