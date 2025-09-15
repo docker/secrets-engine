59bb3d262f86ac72bf02cf04ff4f04b01c123c76
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2025-09-15 11:34:13 +0200

    posixage: lock recovery pattern
    Signed-off-by: Alano Terblanche <18033717+Benehiko@users.noreply.github.com>

--- store/posixage/flock.go
+++ store/posixage/flock.go
@@ -1,8 +1,12 @@
 package posixage
 
 import (
+	"context"
 	"errors"
 	"os"
+	"time"
+
+	"github.com/cenkalti/backoff/v5"
 )
 
 var (
@@ -12,12 +16,90 @@ var (
 
 const lockFileName = ".posixage.lock"
 
+// unlockFunc is the callback function returned by [lockFile]
+// it should always be called inside a defer.
 type unlockFunc func() error
 
+// openFile is a helper function for internal use by [lockFile]
 func openFile(root *os.Root) (*os.File, error) {
-	fl, err := root.OpenFile(lockFileName, os.O_RDWR|os.O_CREATE, 0600)
+	fl, err := root.OpenFile(lockFileName, os.O_RDWR|os.O_CREATE, 0o600)
 	if err != nil {
 		return nil, err
 	}
+
 	return fl, nil
+}
+
+// attemptLock acquires a file-based lock using a temporary file.
+//
+// If the lock file does not exist, it is created. If the lock cannot be
+// acquired immediately, the function retries until the specified timeout
+// is reached.
+//
+// To recover from stale locks (e.g., if a process crashes before releasing),
+// the function removes the lock file if its modification time is older than
+// 30 seconds. This assumes no valid process would hold the lock that long.
+func attemptLock(root *os.Root, exclusive bool, timeout time.Duration) (unlockFunc, error) {
+	fl, err := openFile(root)
+	if err != nil {
+		return nil, err
+	}
+
+	ep := backoff.NewExponentialBackOff()
+	ep.InitialInterval = time.Millisecond * 10
+	ep.MaxInterval = time.Millisecond * 100
+
+	unlock, err := backoff.Retry(context.Background(), func() (unlockFunc, error) {
+		return lockFile(fl, exclusive)
+	},
+		backoff.WithBackOff(ep),
+		backoff.WithMaxElapsedTime(timeout),
+	)
+	if err != nil {
+		// recoverLock closes fl
+		recoverErr := recoverLock(root, fl)
+		if recoverErr != nil && !errors.Is(recoverErr, errRecoverLock) {
+			// we want the underlying error - something went wrong to recover
+			// the file (e.g. could not remove).
+			return nil, errors.Join(err, recoverErr)
+		}
+		if recoverErr != nil && errors.Is(recoverErr, errRecoverLock) {
+			return nil, err
+		}
+
+		// we could recover, re-run the lock
+		return lockFile(fl, exclusive)
+	}
+	return unlock, nil
+}
+
+var errRecoverLock = errors.New("lock file is not older than 30 seconds")
+
+// recoverLock checks whether a lock file's modification time is older than
+// 30 seconds. If so, it removes the file to recover from a stale lock.
+//
+// The file is always closed, since this function is intended to be called
+// as a last resort when acquiring a lock.
+//
+// It returns no error if the lock file was successfully removed.
+func recoverLock(root *os.Root, fl *os.File) error {
+	// always close the file regardless
+	defer func() { _ = fl.Close() }()
+
+	info, err := fl.Stat()
+	if err != nil {
+		return err
+	}
+
+	// the lock file should not have existed for such a long time
+	// it is possible that the application might have crashed before
+	// let's try recover from that so that we don't lock indefinitely.
+	if time.Since(info.ModTime()).Seconds() >= 30 {
+		_ = fl.Close()
+		if err := root.Remove(lockFileName); err != nil {
+			return err
+		}
+		return nil
+	}
+	return errRecoverLock
 }
--- store/posixage/flock_test.go
+++ store/posixage/flock_test.go
@@ -3,26 +3,40 @@ package posixage
 import (
 	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestFlock(t *testing.T) {
+	t.Run("can unlock", func(t *testing.T) {
+		root, err := os.OpenRoot(t.TempDir())
+		require.NoError(t, err)
+
+		exclusive := true
+		unlock, err := attemptLock(root, exclusive, time.Millisecond*10)
+		require.NoError(t, err)
+		require.NoError(t, unlock())
+
+		unlock, err = attemptLock(root, exclusive, time.Millisecond)
+		require.NoError(t, err)
+		require.NoError(t, unlock())
+	})
 	t.Run("exclusive lock prevents others from acquiring lock", func(t *testing.T) {
 		root, err := os.OpenRoot(t.TempDir())
 		require.NoError(t, err)
 
 		exclusive := true
-		unlock, err := lockFile(root, exclusive)
+		unlock, err := attemptLock(root, exclusive, time.Millisecond)
 		require.NoError(t, err)
 		t.Cleanup(func() {
 			_ = unlock()
 		})
 
-		_, err = lockFile(root, exclusive)
+		_, err = attemptLock(root, exclusive, time.Millisecond)
 		require.ErrorIs(t, err, ErrLockUnsuccessful)
 
-		_, err = lockFile(root, !exclusive)
+		_, err = attemptLock(root, !exclusive, time.Millisecond)
 		require.ErrorIs(t, err, ErrLockUnsuccessful)
 	})
 
@@ -31,19 +45,61 @@ func TestFlock(t *testing.T) {
 		require.NoError(t, err)
 
 		exclusive := true
-		unlock, err := lockFile(root, !exclusive)
+		unlock, err := attemptLock(root, !exclusive, time.Millisecond)
 		require.NoError(t, err)
 		t.Cleanup(func() {
 			_ = unlock()
 		})
 
-		unlockTwo, err := lockFile(root, !exclusive)
+		unlockTwo, err := attemptLock(root, !exclusive, time.Millisecond)
 		require.NoError(t, err, ErrLockUnsuccessful)
 		t.Cleanup(func() {
 			_ = unlockTwo()
 		})
 
-		_, err = lockFile(root, exclusive)
+		_, err = attemptLock(root, exclusive, time.Millisecond)
 		require.ErrorIs(t, err, ErrLockUnsuccessful)
+	})
+
+	t.Run("can recover from an exclusive lock", func(t *testing.T) {
+		root, err := os.OpenRoot(t.TempDir())
+		require.NoError(t, err)
+
+		exclusive := true
+		_, err = attemptLock(root, exclusive, time.Millisecond)
+		require.NoError(t, err)
+
+		// change the lock file modification time
+		fakeModTime := time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC)
+		require.NoError(t, root.Chtimes(lockFileName, fakeModTime, fakeModTime))
+
+		unlock, err := attemptLock(root, exclusive, time.Millisecond)
+		require.NoError(t, err)
+		require.NoError(t, unlock())
+	})
+
+	t.Run("recoverLock errors if a recover was not possible", func(t *testing.T) {
+		root, err := os.OpenRoot(t.TempDir())
+		require.NoError(t, err)
+
+		f, err := root.Create(lockFileName)
+		require.NoError(t, err)
+
+		require.ErrorIs(t, recoverLock(root, f), errRecoverLock)
+	})
+
+	t.Run("recoverLock removes the file if it is older than 30 seconds", func(t *testing.T) {
+		root, err := os.OpenRoot(t.TempDir())
+		require.NoError(t, err)
+
+		f, err := root.Create(lockFileName)
+		require.NoError(t, err)
+		// change the lock file modification time
+		fakeModTime := time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC)
+		require.NoError(t, root.Chtimes(lockFileName, fakeModTime, fakeModTime))
+
+		require.NoError(t, recoverLock(root, f))
+		_, err = root.Stat(lockFileName)
+		require.ErrorIs(t, err, os.ErrNotExist)
 	})
 }
