b35b2bc07d7f5ecae253fe54521c2b5d01544bde
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2025-09-19 06:57:24 +0200

    posixage: always unlock mutex on flock error
    Signed-off-by: Alano Terblanche <18033717+Benehiko@users.noreply.github.com>

--- store/posixage/store.go
+++ store/posixage/store.go
@@ -51,6 +51,7 @@ func (f *fileStore[T]) tryLock(ctx context.Context) (func(), error) {
 
 	unlock, err := flock.TryLock(ctx, f.filesystem)
 	if err != nil {
+		f.l.Unlock()
 		return nil, err
 	}
 
@@ -76,6 +77,7 @@ func (f *fileStore[T]) tryRLock(ctx context.Context) (func(), error) {
 
 	unlock, err := flock.TryRLock(ctx, f.filesystem)
 	if err != nil {
+		f.l.RUnlock()
 		return nil, err
 	}
 
