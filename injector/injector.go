33821beec3ce9927ddfe157418a408d4b436f525
Johannes Großmann <grossmann.johannes@t-online.de>
2025-10-16 09:15:53 +0200

    feat(injector): ENV resolver logic

--- injector/injector.go
+++ injector/injector.go
@@ -2,27 +2,86 @@ package injector
 
 import (
 	"context"
+	"errors"
+	"strings"
 
 	"github.com/docker/docker/api/types/container"
 
 	"github.com/docker/secrets-engine/client"
+	"github.com/docker/secrets-engine/x/logging"
 	"github.com/docker/secrets-engine/x/secrets"
 )
 
-type Injector struct {
+const prefix = "se://"
+
+type resolver struct {
+	logger   logging.Logger
 	resolver secrets.Resolver
 }
 
-func New(options ...client.Option) (*Injector, error) {
+func newResolver(logger logging.Logger, options ...client.Option) (*resolver, error) {
 	c, err := client.New(options...)
 	if err != nil {
 		return nil, err
 	}
-	return &Injector{
+	return &resolver{
+		logger:   logger,
 		resolver: c,
 	}, nil
 }
 
-func (i *Injector) ContainerCreateRequestRewrite(context.Context, *container.CreateRequest) error {
+func (r *resolver) resolveENV(ctx context.Context, key, value string) (string, error) {
+	if key == "" {
+		return "", errors.New("key is required")
+	}
+	if value == "" {
+		id, err := secrets.ParseID(key)
+		if err != nil {
+			r.logger.Printf("%s has no value but is not a valid secret ID (%s) -> cannot resolve", key, err)
+			return "", nil
+		}
+		result, err := r.resolver.GetSecrets(ctx, secrets.MustParsePattern(id.String()))
+		if err != nil {
+			if !errors.Is(err, secrets.ErrNotFound) {
+				r.logger.Errorf("resolving ENV %s to secret: %s", key, err)
+			}
+			return "", nil
+		}
+		if len(result) == 0 {
+			return "", nil
+		}
+		return string(result[0].Value), nil
+	}
+	if !strings.HasPrefix(value, prefix) {
+		return value, nil
+	}
+	unprefixedValue := strings.TrimPrefix(value, prefix)
+	pattern, err := secrets.ParsePattern(unprefixedValue)
+	if err != nil {
+		return "", err
+	}
+	result, err := r.resolver.GetSecrets(ctx, pattern)
+	if err != nil {
+		return "", err
+	}
+	if len(result) == 0 {
+		return "", secrets.ErrNotFound
+	}
+	return string(result[0].Value), nil
+}
+
+type ContainCreateRewriter struct {
+	r *resolver
+}
+
+func New(logger logging.Logger, options ...client.Option) (*ContainCreateRewriter, error) {
+	resolver, err := newResolver(logger, options...)
+	if err != nil {
+		return nil, err
+	}
+	return &ContainCreateRewriter{r: resolver}, nil
+}
+
+func (r *ContainCreateRewriter) ContainerCreateRequestRewrite(context.Context, *container.CreateRequest) error {
 	return nil
 }
---
+++ injector/injector_test.go
@@ -0,0 +1,134 @@
+package injector
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/docker/secrets-engine/client"
+	"github.com/docker/secrets-engine/engine"
+	"github.com/docker/secrets-engine/x/api"
+	"github.com/docker/secrets-engine/x/secrets"
+	"github.com/docker/secrets-engine/x/testhelper"
+)
+
+var (
+	mockValidVersion = api.MustNewVersion("v7")
+	mockPatternAny   = secrets.MustParsePattern("**")
+)
+
+var _ engine.Plugin = &mockInternalPlugin{}
+
+type mockInternalPlugin struct {
+	secrets map[secrets.ID]string
+}
+
+func (m mockInternalPlugin) GetSecrets(_ context.Context, pattern secrets.Pattern) ([]secrets.Envelope, error) {
+	var result []secrets.Envelope
+	for id, v := range m.secrets {
+		if pattern.Match(id) {
+			result = append(result, secrets.Envelope{ID: id, Value: []byte(v)})
+		}
+	}
+	return result, nil
+}
+
+func (m mockInternalPlugin) Run(ctx context.Context) error {
+	<-ctx.Done()
+	return nil
+}
+
+func testEngine(t *testing.T) string {
+	t.Helper()
+	socketPath := testhelper.RandomShortSocketName()
+	errEngine := make(chan error)
+	up := make(chan struct{})
+	go func() {
+		errEngine <- engine.Run(t.Context(), "test-engine", "test-version",
+			engine.WithLogger(testhelper.TestLogger(t)),
+			engine.WithSocketPath(socketPath),
+			engine.WithEngineLaunchedPluginsDisabled(),
+			engine.WithExternallyLaunchedPluginsDisabled(),
+			engine.WithPlugins(map[engine.Config]engine.Plugin{
+				{Name: "my-builtin", Version: mockValidVersion, Pattern: mockPatternAny}: &mockInternalPlugin{secrets: map[secrets.ID]string{
+					secrets.MustParseID("1password/my-secret"): "some-value",
+					secrets.MustParseID("FOO"):                 "bar",
+				}},
+				{Name: "alphabetic-first-plugin", Version: mockValidVersion, Pattern: mockPatternAny}: &mockInternalPlugin{secrets: map[secrets.ID]string{
+					secrets.MustParseID("vault/my-secret"): "some-other-value",
+				}},
+			}),
+			engine.WithAfterHealthyHook(func(context.Context) error {
+				close(up)
+				return nil
+			}),
+		)
+	}()
+	t.Cleanup(func() { assert.NoError(t, testhelper.WaitForErrorWithTimeout(errEngine)) })
+	require.NoError(t, testhelper.WaitForClosedWithTimeout(up))
+	return socketPath
+}
+
+func Test_resolveENV(t *testing.T) {
+	tests := []struct {
+		name   string
+		key    string
+		value  string
+		result string
+		err    error
+	}{
+		{
+			name: "no value but no secret",
+			key:  "GH_TOKEN",
+		},
+		{
+			name:   "no value and secret",
+			key:    "FOO",
+			result: "bar",
+		},
+		{
+			name: "no value but invalid key",
+			key:  "MY/VAR",
+		},
+		{
+			name:   "value but not a secrets engine path",
+			key:    "GH_TOKEN",
+			value:  "some-value",
+			result: "some-value",
+		},
+		{
+			name:  "value but invalid path",
+			key:   "GH_TOKEN",
+			value: "se://my//path",
+			err:   secrets.ErrInvalidPattern,
+		},
+		{
+			name:  "value but no secret",
+			key:   "GH_TOKEN",
+			value: "se://bar",
+			err:   secrets.ErrNotFound,
+		},
+		{
+			name:   "value and first matched secret",
+			key:    "GH_TOKEN",
+			value:  "se://*/my-secret",
+			result: "some-other-value",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			socketPath := testEngine(t)
+			r, err := newResolver(testhelper.TestLogger(t), client.WithSocketPath(socketPath))
+			require.NoError(t, err)
+			value, err := r.resolveENV(t.Context(), tt.key, tt.value)
+			if tt.err != nil {
+				assert.Error(t, err)
+				return
+			}
+			assert.NoError(t, err)
+			assert.Equal(t, tt.result, value)
+		})
+	}
+}
