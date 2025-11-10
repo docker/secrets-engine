26055a757092f11bed8d2f27b3ec74c5121da5ec
Johannes Großmann <grossmann.johannes@t-online.de>
2025-11-10 10:24:16 +0100

    feat(pass): make store configurable

---
+++ pass/store/store.go
@@ -0,0 +1,40 @@
+package store
+
+import (
+	"github.com/docker/secrets-engine/store"
+	"github.com/docker/secrets-engine/store/keychain"
+)
+
+var _ store.Secret = &PassValue{}
+
+type PassValue struct {
+	Value []byte `json:"value"`
+}
+
+func (m *PassValue) Marshal() ([]byte, error) {
+	return m.Value, nil
+}
+
+func (m *PassValue) Unmarshal(data []byte) error {
+	m.Value = data
+	return nil
+}
+
+func (m *PassValue) Metadata() map[string]string {
+	return nil
+}
+
+func (m *PassValue) SetMetadata(map[string]string) error {
+	return nil
+}
+
+func PassStore(serviceGroup string) (store.Store, error) {
+	kc, err := keychain.New(
+		serviceGroup,
+		"docker-pass-cli",
+		func() *PassValue {
+			return &PassValue{}
+		},
+	)
+	return kc, err
+}
